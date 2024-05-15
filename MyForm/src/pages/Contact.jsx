c298662ce52eb197f3d96c0f10ce5a081c196a54
fayazkhan <fayazkhan35119@gmail.com>
2024-05-15 09:16:16 +0500

    useState, onChange in Contact page

--- MyForm/src/pages/Contact.jsx
+++ MyForm/src/pages/Contact.jsx
@@ -1,27 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+
+
 
 function Contact() {
+
+
+  let [name, setName] = useState('');
+  let [email, setEmail] = useState("");
+  let [city, setCity] = useState("");
+  let [message, setMessage] = useState("");
+  
+  
+  let handleSubmit = (event)=>{
+    event.preventDefault();
+    console.log(
+      {
+        name,
+        email,
+        city,
+        message
+      }
+    )
+  }
+  
+
+
   return (
     <div>
-      <form className="row g-3 w-50 py-5">
+      <form className="row g-3 w-50 py-5" onSubmit={handleSubmit}>
         <div className="col-md-12 ">
           <label htmlFor="fulName" className="form-label">
             Full Name
           </label>
-          <input type="text" className="form-control" id="fullName" />
+          <input type="text" onChange={(event)=>setName(event.target.value)} className="form-control" id="fullName" />
         </div>
         <div className="col-md-12">
           <label htmlFor="email" className="form-label">
             Email
           </label>
-          <input type="text" className="form-control" id="email" />
+          <input type="text" onChange={(event)=>setEmail(event.target.value)} className="form-control" id="email" />
         </div>
 
         <div className="col-md-12">
           <label htmlFor="city" className="form-label">
             City
           </label>
-          <input type="text" className="form-control" id="city" />
+          <input type="text" onChange={(event)=>setCity(event.target.value)} className="form-control" id="city" />
         </div>
 
         <div className="mb-3">
@@ -29,6 +53,7 @@ function Contact() {
             Message
           </label>
           <textarea
+            onChange={(event)=>setMessage(event.target.value)}
             className="form-control"
             id="message"
             rows="3"
