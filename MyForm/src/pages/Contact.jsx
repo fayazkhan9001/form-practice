2265a521ea7ec093b875e3f843b092f7ba157471
fayazkhan <fayazkhan35119@gmail.com>
2024-05-16 00:09:30 +0500

    get Data from local storage with help of useEffect{side effect}

--- MyForm/src/pages/Contact.jsx
+++ MyForm/src/pages/Contact.jsx
@@ -1,51 +1,100 @@
-import React, { useState } from "react";
-
-
+import React, { useEffect, useState } from "react";
 
 function Contact() {
+  const [contact, setContact] = useState({
+    fullName: "",
+    email: "",
+    city: "",
+    message: "",
+  });
 
+  const [con, setCon] = useState([]);
 
-  const [contact, setContact] = useState(
-      {
-        fullName : '',
-        email : '',
-        city : '',
-        message : '',
-      }
-  );
-  
-  let handleSubmit = (event)=>{
+  const [message, setMessage] = useState({
+    success: false,
+    message: "",
+  });
+
+  useEffect(() => {
+    setCon(JSON.parse(localStorage.getItem("contact")));
+  }, []);
+
+  let handleSubmit = (event) => {
     event.preventDefault();
-    console.log(
-      {
-        contact
-      }
-    )
-  }
-  
+    localStorage.setItem("contact", JSON.stringify(contact));
 
+    setMessage({
+      success: true,
+      message: " your data has been saved.",
+    });
+
+    setContact({
+      fullName: "",
+      email: "",
+      city: "",
+      message: "",
+    });
+  };
 
   return (
-    <div>
+    <div className="container">
+      {message.success ? (
+        <div
+          className="alert alert-warning alert-dismissible fade show"
+          role="alert"
+        >
+          <strong> Message! </strong> {message.message}
+          <button
+            type="button"
+            className="btn-close"
+            data-bs-dismiss="alert"
+            aria-label="Close"
+          ></button>
+        </div>
+      ) : null}
       <form className="row g-3 w-50 py-5" onSubmit={handleSubmit}>
         <div className="col-md-12 ">
           <label htmlFor="fulName" className="form-label">
             Full Name
           </label>
-          <input type="text" onChange={(event)=>setContact({...contact, fullName:event.target.value})} className="form-control" id="fullName" />
+          <input
+            type="text"
+            onChange={(event) =>
+              setContact({ ...contact, fullName: event.target.value })
+            }
+            value={contact.fullName}
+            className="form-control"
+            id="fullName"
+          />
         </div>
         <div className="col-md-12">
           <label htmlFor="email" className="form-label">
             Email
           </label>
-          <input type="text" onChange={(event)=>setContact({...contact, email:event.target.value})} className="form-control" id="email" />
+          <input
+            type="text"
+            onChange={(event) =>
+              setContact({ ...contact, email: event.target.value })
+            }
+            value={contact.email}
+            className="form-control"
+            id="email"
+          />
         </div>
 
         <div className="col-md-12">
           <label htmlFor="city" className="form-label">
             City
           </label>
-          <input type="text" onChange={(event)=>setContact({...contact, city:event.target.value})} className="form-control" id="city" />
+          <input
+            type="text"
+            onChange={(event) =>
+              setContact({ ...contact, city: event.target.value })
+            }
+            value={contact.city}
+            className="form-control"
+            id="city"
+          />
         </div>
 
         <div className="mb-3">
@@ -53,7 +102,10 @@ function Contact() {
             Message
           </label>
           <textarea
-            onChange={(event)=>setContact({...contact, message:event.target.value})}
+            onChange={(event) =>
+              setContact({ ...contact, message: event.target.value })
+            }
+            value={contact.message}
             className="form-control"
             id="message"
             rows="3"
@@ -66,6 +118,25 @@ function Contact() {
           </button>
         </div>
       </form>
+
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">City</th>
+            <th scope="col">Message</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">{con.fullName}</th>
+            <td>{con.email}</td>
+            <td>{con.city}</td>
+            <td>{con.message}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
